31f37c1c0c2bf8c8cbb0fa3517e867fddf38a42c
luckyy <smashbrosultimate2025@gmail.com>
2026-08-06 23:04:52 -0300

    Add files via upload

---
+++ types/local-check.d.ts
@@ -0,0 +1,23 @@
+declare module "@vendetta/metro" {
+    export const findByName: any;
+    export const findByProps: any;
+    export const findByStoreName: any;
+}
+declare module "@vendetta/metro/common" {
+    export const FluxDispatcher: any;
+    export const i18n: any;
+    export const ReactNative: any;
+}
+declare module "@vendetta/patcher" {
+    export const after: any;
+    export const before: any;
+}
+declare module "@vendetta/ui/assets" {
+    export const getAssetIDByName: any;
+}
+declare module "@vendetta/ui/components" {
+    export const Forms: any;
+}
+declare module "@vendetta/utils" {
+    export const findInReactTree: any;
+}
